6b654d1e62aa32b40d2e34533862f779fbc5d708
Kevin Sheng <ksheng92@gmail.com>
2012-10-05 02:03:29 -0700

    Fixed dollar amount handling

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -5,20 +5,23 @@ class DonationsController < ApplicationController
   def create
     Stripe.api_key = "sk_giwOujcUI3OferKZpqb9fttrVbCnK"
 
-    @token = params[:stripeToken]
-    @name = params[:name]
-    @amount = params[:amount].to_i
-    @dollar_amount = "#{@amount / 100}.#{@amount - ((@amount / 100) * 100)}"
-    @company = params[:company]
-    @description = params[:description]
+    token = params[:stripeToken]
+    name = params[:name]
+    company = params[:company]
+    description = params[:description]
 
-    charge = Stripe::Charge.create(:amount => @amount, :card => @token, :description => "#{@company}: #{@description}", :currency => 'usd')
+    @dollar_amount = params[:amount]
+    dollars, cents = @dollar_amount.split('.')
+    amount = (dollars.to_i * 100) + cents.to_i
+
+    charge = Stripe::Charge.create(:amount => amount, :card => token, :description => "#{company}: #{description}", :currency => 'usd')
 
     if charge
-      Donation.create(:name => @name, :company => @company, :amount => @amount, :description => @description)
+      Donation.create(:name => name, :company => company, :amount => amount, :description => description)
     end
 
   rescue Stripe::CardError => e
+    # TODO catch this message and print custom one?
     flash[:error] = e.message
     redirect_to new_donation_path
   end
