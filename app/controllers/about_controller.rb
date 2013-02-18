d9ba7c8772e3fc2d72ecce2c3e32fe177b403a53
Eleane Jao <eleanew83@berkeley.edu>
2013-02-17 21:40:09 -0800

    Change Officer Page: pix and names.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -8,13 +8,21 @@ class AboutController < ApplicationController
 
   def officers
     @execs = [['Jeff Tsui', 'Chair'], ['Noel Moldvai', 'External VP'], ['Ayushi Samaddar', 'Internal VP'], ['Guillaume Bellegarda', 'Treasurer'], ['Annie Shin', 'Secretary']]
-    @activities = ['Sean Soleyman', 'Alice Lee', 'Ino Samaras', 'Leland Au', 'Michael Chen', 'Rohan Sachdev', 'Mano Pagalavan']
-    @hope = ['Jeff Lievense', 'Andrew Wun', 'Milad Bayan', 'Rongchang Lei', 'James Yang']
-    @indrel = ['Vikram Iyer', 'Jonathan Hsu', 'Marion Halim', 'Kevin Chian', 'Jamshed Vesuna', 'Harrison Tsai', "Alton Zheng"]
-    @io = ['Nate Holland', 'Lauren Janney', 'Flora Dai', 'Anthony Sutardja', 'Tabitha King']
-    @prodev = ['Skyler Rojas', 'Parul Singh', 'Michael Romain', 'Andrew Qin', 'Yerin Kim']
-    @techops = ['Felix Wong',  'Alex Romano', 'William Ku', 'Nelson Zhang', 'Eugene Wong']
-    @website = ['Kevin Sheng', 'Yuval Gnessin', 'Tom McCormick', 'Steve Yadlowsky', 'Jessika Wu', 'David Su']
+    @activities = ['Alice Lee', 'Rohan Sachdev', 'Krishna Parashar', 'Ino Samaras', 'Lori Krakirian', 'William Ku', 'Leland Au']
+    @hope = ['Jeff Lievense', 'Andrew Wun', 'James Yang', 'Rishi Sharma', 'Gokul Ramesh', 'Rongchang Lei', 'Ronald Lee']
+    @indrel = ['Vikram Iyer', 'Harrison Tsai', 'Marion Halim', 'Jamshed Vesuna', 'Luca Zuccarini', 'Kevin Chian', 'Jonathan Hsu']
+    @io = ['Flora Dai', 'Lauren Janney', 'Tabitha King', 'Ingrid Hong', 'Sihyung Kim', 'Dorothy Jung']
+    @prodev = ['Skyler Rojas', 'Michael Romain', 'Parul Singh', 'Paymon Payamipour', 'Colin Elder', 'Nate Holland']
+    @techops = ['Eugene Wong',  'Preetum Nakkiran', 'Harish Shanker', 'Eric Wu', 'Tom McCormick', 'Alex Romano', 'Felix Wong']
+    @website = ['Yuval Gnessin', 'Pranava Adduri', 'Kevin Sheng', 'David Su', 'Anuvind Menon', 'Eleane Jao', 'Jimmy Wu']
+	
+#	 @activities = ['Sean Soleyman', 'Alice Lee', 'Ino Samaras', 'Leland Au', 'Michael Chen', 'Rohan Sachdev', 'Mano Pagalavan']
+#    @hope = ['Jeff Lievense', 'Andrew Wun', 'Milad Bayan', 'Rongchang Lei', 'James Yang']
+#    @indrel = ['Vikram Iyer', 'Jonathan Hsu', 'Marion Halim', 'Kevin Chian', 'Jamshed Vesuna', 'Harrison Tsai', "Alton Zheng"]
+#    @io = ['Nate Holland', 'Lauren Janney', 'Flora Dai', 'Anthony Sutardja', 'Tabitha King']
+#    @prodev = ['Skyler Rojas', 'Parul Singh', 'Michael Romain', 'Andrew Qin', 'Yerin Kim']
+#    @techops = ['Felix Wong',  'Alex Romano', 'William Ku', 'Nelson Zhang', 'Eugene Wong']
+#    @website = ['Kevin Sheng', 'Yuval Gnessin', 'Tom McCormick', 'Steve Yadlowsky', 'Jessika Wu', 'David Su']
 
 #    @execs = [['Tianbo Xu', 'Chair'], ['Pranava Adduri', 'External VP'], ['Mark Hettick', 'Internal VP'], ['Gabe Fierro', 'Secretary'], ['Jeff Tsui', 'Treasurer']]
 #    @activities = ['Leland Au', 'Alice Lee', 'Amanda Ren', 'Annie Shin', 'Edward Wu']
