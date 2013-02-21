5d5bd8ed113e14dcf13feee905745696cf47f3d7
Yuval Gnessin <ygnessin@gmail.com>
2013-02-21 15:39:16 -0800

    fixed default-photo bug on officer page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -7,6 +7,8 @@ class AboutController < ApplicationController
   end
 
   def officers
+    require 'pathname'
+    
     @execs = [['Jeff Tsui', 'Chair'], ['Noel Moldvai', 'External VP'], ['Ayushi Samaddar', 'Internal VP'], ['Guillaume Bellegarda', 'Treasurer'], ['Annie Shin', 'Secretary']]
     @activities = ['Alice Lee', 'Rohan Sachdev', 'Krishna Parashar', 'Ino Samaras', 'Lori Krakirian', 'William Ku', 'Leland Au']
     @hope = ['Jeff Lievense', 'Andrew Wun', 'James Yang', 'Rishi Sharma', 'Gokul Ramesh', 'Rongchang Lei', 'Ronald Lee']
@@ -15,23 +17,6 @@ class AboutController < ApplicationController
     @prodev = ['Skyler Rojas', 'Michael Romain', 'Parul Singh', 'Paymon Payamipour', 'Colin Elder', 'Nate Holland']
     @techops = ['Eugene Wong',  'Preetum Nakkiran', 'Harish Shanker', 'Eric Wu', 'Tom McCormick', 'Alex Romano', 'Felix Wong']
     @website = ['Yuval Gnessin', 'Pranava Adduri', 'Kevin Sheng', 'David Su', 'Anuvind Menon', 'Eleane Jao', 'Jimmy Wu']
-	
-#	 @activities = ['Sean Soleyman', 'Alice Lee', 'Ino Samaras', 'Leland Au', 'Michael Chen', 'Rohan Sachdev', 'Mano Pagalavan']
-#    @hope = ['Jeff Lievense', 'Andrew Wun', 'Milad Bayan', 'Rongchang Lei', 'James Yang']
-#    @indrel = ['Vikram Iyer', 'Jonathan Hsu', 'Marion Halim', 'Kevin Chian', 'Jamshed Vesuna', 'Harrison Tsai', "Alton Zheng"]
-#    @io = ['Nate Holland', 'Lauren Janney', 'Flora Dai', 'Anthony Sutardja', 'Tabitha King']
-#    @prodev = ['Skyler Rojas', 'Parul Singh', 'Michael Romain', 'Andrew Qin', 'Yerin Kim']
-#    @techops = ['Felix Wong',  'Alex Romano', 'William Ku', 'Nelson Zhang', 'Eugene Wong']
-#    @website = ['Kevin Sheng', 'Yuval Gnessin', 'Tom McCormick', 'Steve Yadlowsky', 'Jessika Wu', 'David Su']
-
-#    @execs = [['Tianbo Xu', 'Chair'], ['Pranava Adduri', 'External VP'], ['Mark Hettick', 'Internal VP'], ['Gabe Fierro', 'Secretary'], ['Jeff Tsui', 'Treasurer']]
-#    @activities = ['Leland Au', 'Alice Lee', 'Amanda Ren', 'Annie Shin', 'Edward Wu']
-#    @hope = ['Sean Soleyman', 'Oliver Zhu', 'Andrew Wun']
-#    @indrel = ['Mihir Joshi', 'Benjamin Lui', 'Noel Moldvai', 'Amudhan Venkatesan', 'Mano Pagalavan']
-#    @io = ['Jaclyn Louie', 'Navin Eluthesen', 'Ayushi Samaddar', 'Jessika Wu', 'Muller Zhang']
-#    @prodev = ['Owen Lin', 'Deirdre Chen', 'Aime Ngongang', 'Jeremy Williams']
-#    @techops = ['Felix Wong', 'Guillaume Bellegarda', 'Sergio Hidalgo', 'Jonathan Hsu', 'William Ku', 'Sam Whitlock']
-#    @website = ['Kristine Chen', 'Jian P. Chen', 'Stephanie Chou', 'Yuval Gnessin', 'Kevin Sheng', 'David Su']
   end
 
   def advisor
