f78184de423444e62695398c0e2748e00c598066
Yuval Gnessin <ygnessin@gmail.com>
2013-02-24 23:01:10 -0800

    added missing officer pictures. Also, updated schema.rb

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -10,13 +10,13 @@ class AboutController < ApplicationController
     require 'pathname'
     
     @execs = [['Jeff Tsui', 'Chair'], ['Noel Moldvai', 'External VP'], ['Ayushi Samaddar', 'Internal VP'], ['Guillaume Bellegarda', 'Treasurer'], ['Annie Shin', 'Secretary']]
-    @activities = ['Alice Lee', 'Rohan Sachdev', 'Krishna Parashar', 'Ino Samaras', 'Lori Krakirian', 'William Ku', 'Leland Au']
-    @hope = ['Jeff Lievense', 'Andrew Wun', 'James Yang', 'Rishi Sharma', 'Gokul Ramesh', 'Rongchang Lei', 'Ronald Lee']
+    @activities = ['Alice Lee', 'Rohan Sachdev', 'Ino Samaras', 'Lori Krakirian', 'William Ku', 'Leland Au', 'Krishna Parashar']
+    @hope = ['Jeff Lievense', 'James Yang', 'Gokul Ramesh', 'Rongchang Lei', 'Ronald Lee', 'Rishi Sharma', 'Andrew Wun']
     @indrel = ['Vikram Iyer', 'Harrison Tsai', 'Marion Halim', 'Jamshed Vesuna', 'Luca Zuccarini', 'Kevin Chian', 'Jonathan Hsu']
-    @io = ['Flora Dai', 'Lauren Janney', 'Tabitha King', 'Ingrid Hong', 'Sihyung Kim', 'Dorothy Jung']
-    @prodev = ['Skyler Rojas', 'Michael Romain', 'Parul Singh', 'Paymon Payamipour', 'Colin Elder', 'Nate Holland']
-    @techops = ['Eugene Wong',  'Preetum Nakkiran', 'Harish Shanker', 'Eric Wu', 'Tom McCormick', 'Alex Romano', 'Felix Wong']
-    @website = ['Yuval Gnessin', 'Pranava Adduri', 'Kevin Sheng', 'David Su', 'Anuvind Menon', 'Eleane Jao', 'Jimmy Wu']
+    @io = ['Flora Dai', 'Lauren Janney', 'Tabitha King', 'Ingrid Hong', 'Sarah Kim', 'Dorothy Jung']
+    @prodev = ['Skyler Rojas', 'Michael Romain', 'Parul Singh', 'Paymon Payamipour', 'Nate Holland', 'Colin Elder']
+    @techops = ['Eugene Wong', 'Harish Shanker', 'Eric Wu', 'Tom McCormick', 'Alex Romano', 'Felix Wong', 'Preetum Nakkiran']
+    @website = ['Yuval Gnessin', 'Kevin Sheng', 'David Su', 'Anuvind Menon', 'Eleane Jao', 'Jimmy Wu', 'Pranava Adduri']
   end
 
   def advisor
